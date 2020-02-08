0d7fe3f37972d481ca9651dbd94e701d3c885271
Sanket Landge <sanketlandge57@gmail.com>
2020-02-08 12:05:04 +0530

    leaderboard and submission views

--- UserApp/views.py
+++ UserApp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render,redirect,reverse
+from django.http import HttpResponse
+from rest_framework.permissions import IsAuthenticated
 from .models import *
 from .serializer import *
 from rest_framework.views import APIView
@@ -8,6 +10,9 @@ from knox.models import AuthToken
 
 
 class signup(APIView):
+    def get(self,request):
+        return HttpResponse("This is Login Page")
+
     def post(self,request):
         username = request.data.get("username")
         password = request.data.get('password')
@@ -23,6 +28,44 @@ class signup(APIView):
         userprofile = UserProfile(user=user,email1=email1,email2=email2,name1=name1,name2=name2,phone1=phone1,phone2=phone2)
         userprofile.save()
 
-
-
         return Response({"data": request.data , "token": AuthToken.objects.create(user)[1] },status=201)
+
+
+
+
+
+class LaderBoard(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self,request):
+        data = {}
+
+        for player in UserProfile.objects.order_by("-totalScore"):
+            l = []
+            for i in range(1,7):
+                que = Question.objects.get(pk=i)
+                try:
+                    ans = MultipleQues.objects.get(user=player,que=que)
+                    l.append(ans.scoreQuestion)
+                except MultipleQues.DoesNotExist:
+                    l.append(0)
+            l.append(player.totalScore)
+            data[player.user] = l
+
+        sorted(data.items(), key= lambda x: (x[1][6], player.latestSubTime) )
+        return Response({"data": data})
+
+
+class Submissions(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self,request,qn):
+        que = Question.objects.get(pk=qn)
+        total_submissions = Submission.objects.filter(user=request.user)
+        usersub = []
+
+        for submission in total_submissions:
+            if(submission.que == que):
+                usersub.append(submission)
+
+        return Response({"submissions": usersub})
