18af16b9658b1ca4ff8a3d0c54d0c6a7faadbeee
tanmaypardeshi <tanmaypardeshi@gmail.com>
2020-02-08 15:54:50 +0530

    Question hub. Please check

--- UserApp/urls.py
+++ UserApp/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path,include
+from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('',include('knox.urls')),
-    path('signup/',signup.as_view(),name='signup'),
+    path('', include('knox.urls')),
+    path('signup/', Signup.as_view(), name='signup'),
 ]
--- UserApp/views.py
+++ UserApp/views.py
@@ -1,19 +1,32 @@
-from django.shortcuts import render,redirect,reverse
 from django.http import HttpResponse
 from rest_framework.permissions import IsAuthenticated
-from .models import *
 from .serializer import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import permissions
 from knox.models import AuthToken
+from datetime import datetime
+
+starttime = 0
+end_time = 0
 
 
-class signup(APIView):
-    def get(self,request):
+def calculate():
+    time = datetime.datetime.now()
+    nowsec = (time.hour * 60 * 60) + (time.minute * 60) + time.second
+    global starttime
+    global end_time
+    diff = end_time - nowsec
+    if nowsec < end_time:
+        return diff
+    else:
+        return
+
+
+class Signup(APIView):
+    def get(self, request):
         return HttpResponse("This is Login Page")
 
-    def post(self,request):
+    def post(self, request):
         username = request.data.get("username")
         password = request.data.get('password')
         email1 = request.data.get('email1')
@@ -23,49 +36,79 @@ class signup(APIView):
         phone1 = request.data.get('phone1')
         phone2 = request.data.get('phone2')
 
-        user = User.objects.create_user(username=username,password=password)
+        user = User.objects.create_user(username=username, password=password)
 
-        userprofile = UserProfile(user=user,email1=email1,email2=email2,name1=name1,name2=name2,phone1=phone1,phone2=phone2)
+        userprofile = UserProfile(user=user, email1=email1, email2=email2, name1=name1, name2=name2, phone1=phone1,
+                                  phone2=phone2)
         userprofile.save()
 
-        return Response({"data": request.data , "token": AuthToken.objects.create(user)[1] },status=201)
+        return Response({"data": request.data, "token": AuthToken.objects.create(user)[1]}, status=201)
 
 
-
-
-
-class LaderBoard(APIView):
+class LeaderBoard(APIView):
     permission_classes = [IsAuthenticated]
 
-    def get(self,request):
+    def get(self, request):
         data = {}
 
         for player in UserProfile.objects.order_by("-totalScore"):
             l = []
-            for i in range(1,7):
+            for i in range(1, 7):
                 que = Question.objects.get(pk=i)
                 try:
-                    ans = MultipleQues.objects.get(user=player,que=que)
+                    ans = MultipleQues.objects.get(user=player, que=que)
                     l.append(ans.scoreQuestion)
                 except MultipleQues.DoesNotExist:
                     l.append(0)
             l.append(player.totalScore)
             data[player.user] = l
 
-        sorted(data.items(), key= lambda x: (x[1][6], player.latestSubTime) )
+        sorted(data.items(), key=lambda x: (x[1][6], player.latestSubTime))
         return Response({"data": data})
 
 
 class Submissions(APIView):
     permission_classes = [IsAuthenticated]
 
-    def get(self,request,qn):
+    def get(self, request, qn):
         que = Question.objects.get(pk=qn)
         total_submissions = Submission.objects.filter(user=request.user)
         usersub = []
 
         for submission in total_submissions:
-            if(submission.que == que):
+            if (submission.que == que):
                 usersub.append(submission)
 
         return Response({"submissions": usersub})
+
+
+class Questionhub(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        try:
+            user_profile = UserProfile.objects.get(user=request.user)
+        except UserProfile.DoesNotExist:
+            return HttpResponse("User profile does not exist")
+
+        all_questions = Question.objects.all()
+        all_users = User.objects.all()
+
+        for que in all_questions:
+            for user in all_users:
+                try:
+                    mul_que = MultipleQues.objects.get(user=user, que=que)
+                except MultipleQues.DoesNotExist:
+                    mul_que = MultipleQues(user=user, que=que)
+                que.totalSub += 1 if mul_que.attempts > 0 else 0
+
+            try:
+                que.accuracy = round((que.totalSuccessfulSub * 100 / que.totalSub), 1)
+            except ZeroDivisionError:
+                que.accuracy = 0
+        var = calculate()
+        if var != 0:
+            return Response({"all_questions": all_questions , 'time': var })
+
+
+
