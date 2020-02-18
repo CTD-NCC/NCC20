57a1ca1c0fdb4da844f8d307fe671e97235f35f1
gauravghati <gauravghati225@gmail.com>
2020-02-18 20:01:55 +0530

    authentication

--- UserApp/views.py
+++ UserApp/views.py
@@ -1,22 +1,25 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, reverse
 from rest_framework.permissions import IsAuthenticated
 from .serializer import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.http import JsonResponse,HttpResponse
-from knox.models import AuthToken
+from django.http import JsonResponse, HttpResponse
 from datetime import datetime
 import datetime, os, re
 from seccomp.views import exec_main
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
 
 starttime = 0
 end_time = 0
 duration = 0
 start = datetime.datetime(2020, 1, 1, 0, 0)
+flag = False
 
 pathusercode = 'data/usersCode/'
 standard = 'data/standard/'
 NO_OF_TEST_CASES = 6
+
 
 def timer():
     global starttime, start
@@ -46,8 +49,9 @@ def calculate():
 
 class Signup(APIView):
     def get(self, request):
-        timer()
-        return HttpResponse("This is Login Page")
+        if request.user.is_authenticated:
+            return redirect(reverse('questionhub'))
+        return render(request, 'index.html')
 
     def post(self, request):
         username = request.POST("username")
@@ -65,220 +69,251 @@ class Signup(APIView):
         userprofile = UserProfile(user=user, email1=email1, email2=email2, name1=name1, name2=name2, phone1=phone1,
                                   phone2=phone2,junior=junior)
         userprofile.save()
+        login(request, user)
         os.system(f'mkdir {pathusercode}/{username}')
+
         return Response({"data": request.data}, status=201)
 
 
 class Code(APIView):
     def get(self, request, qn):
-        question = Question.objects.get(pk=qn)
-        que_title = question.titleQue
-        que = question.question
-        user = User.objects.get(user=request.user)
+        if request.user.is_authenticated:
+            question = Question.objects.get(pk=qn)
+            que_title = question.titleQue
+            que = question.question
+            user = User.objects.get(user=request.user)
 
-        var = calculate()
-        data = {
-            "user": user.username,
-            "question_title": que_title,
-            "questin": que,
-            "total": user.totalScore,
-            "time": var
-        }
-        return JsonResponse(data)
+            var = calculate()
+            data = {
+                "user": user.username,
+                "question_title": que_title,
+                "questin": que,
+                "total": user.totalScore,
+                "time": var
+            }
+            return JsonResponse(data)
+        else:
+            return redirect(reverse('singup'))
 
-    def post(self,request,qn):
-        question = Question.objects.get(pk=qn)
-        usr = request.user
-        userprof = UserProfile.objects.get(user=usr)
-        username = userprof.user.username
-        content = request.POST['content']
-        ext = request.POST['ext']
+    def post(self, request, qn):
+        if request.user.is_authenticated:
+            question = Question.objects.get(pk=qn)
+            usr = request.user
+            userprof = UserProfile.objects.get(user=usr)
+            username = userprof.user.username
+            content = request.POST['content']
+            ext = request.POST['ext']
+            runflag = request.POST['runflag']
 
-        try:
-            mulque = MultipleQues.objects.get(user=usr,que=question)
-        except MultipleQues.DoesNotExist:
-            mulque = MultipleQues(user=usr,que=question)
-        att = mulque.attempts
-        user_code_path = f"{pathusercode}/{username}/question{qn}"
-        if not os.path.exists(user_code_path):
-            os.system(f"mkdir {user_code_path}")
+            try:
+                mulque = MultipleQues.objects.get(user=usr, que=question)
+            except MultipleQues.DoesNotExist:
+                mulque = MultipleQues(user=usr, que=question)
+            att = mulque.attempts
 
-        codefile  = user_code_path + f"code{att}.{ext}"
-        content = str(content)
+            user_code_path = f"{pathusercode}/{username}/question{qn}"
 
-        change_file_content(content,ext,codefile)
+            if not os.path.exists(user_code_path):
+                os.system(f"mkdir {user_code_path}")
 
-        testcase_values = exec_main(
-            username=username,
-            qno = qn,
-            attempts=att,
-            lang=ext
-        )
+            codefile  = user_code_path + f"code{att}.{ext}"
+            content = str(content)
 
-        code_f = open(codefile, "w+")
-        code_f.seek(0)
-        codefile.write(content)
-        code_f.close()
+            change_file_content(content, ext, codefile)
 
-        now_time = datetime.datetime.now()
-        now_time_sec = now_time.second + now_time.minute * 60 + now_time.hour * 60 * 60
-        global starttime
-        submit_Time = now_time_sec - starttime
+            testcase_values = exec_main(
+                username=username,
+                qno=qn,
+                attempts=att,
+                lang=ext,
+                run=runflag
+            )
 
-        hour = submit_Time // (60 * 60)
-        val = submit_Time % (60 * 60)
-        min = val // 60
-        sec = val % 60
+            code_f = open(codefile, "w+")
+            code_f.seek(0)
+            codefile.write(content)
+            code_f.close()
 
-        subTime = f'{hour}:{min}:{sec}'
+            now_time = datetime.datetime.now()
+            now_time_sec = now_time.second + now_time.minute * 60 + now_time.hour * 60 * 60
+            global starttime
+            submit_Time = now_time_sec - starttime
 
-        sub = Submission(code=content, user=usr, que=question, attempt=att, subTime=subTime)
+            hour = submit_Time // (60 * 60)
+            val = submit_Time % (60 * 60)
+            min = val // 60
+            sec = val % 60
 
-        mulque.attempts += 1
-        mulque.save()
+            subTime = f'{hour}:{min}:{sec}'
 
-        error_text = ""
-        epath = pathusercode + f"/{username}/question{qn}/error.txt"
+            sub = Submission(code=content, user=usr, que=question, attempt=att, subTime=subTime)
 
-        if os.path.exists(epath):
-            err = open(epath,"r")
-            error_text = err.read()
-            error_text = re.sub('/.*?:','',error_text)
-            err.close()
+            mulque.attempts += 1
+            mulque.save()
 
-        no_of_pass = 0
-        for i in testcase_values:
-            if i == 'AC':
-                no_of_pass += 1
+            error_text = ""
+            epath = pathusercode + f"/{username}/question{qn}/error.txt"
 
-        sub.correctTestCases = no_of_pass
-        sub.TestCasesPercentage = (no_of_pass / NO_OF_TEST_CASES) * 100
-        sub.save()
+            if os.path.exists(epath):
+                err = open(epath,"r")
+                error_text = err.read()
+                error_text = re.sub('/.*?:','',error_text)
+                err.close()
 
-        status = 'AC' if no_of_pass == NO_OF_TEST_CASES else 'WA'  # overall Status
-        sub.subStatus = status
+            no_of_pass = 0
+            for i in testcase_values:
+                if i == 'AC':
+                    no_of_pass += 1
 
-        if status == 'AC':
-            userprof.totalScore += 100
-            question.totalSuccessfulSub += 1
-            question.totalSub += 1
-            sub.subScore = 100
-            mulque.scoreQuestion = 100
+            sub.correctTestCases = no_of_pass
+            sub.TestCasesPercentage = (no_of_pass / NO_OF_TEST_CASES) * 100
+            sub.save()
+
+            status = 'AC' if no_of_pass == NO_OF_TEST_CASES else 'WA'  # overall Status
+            sub.subStatus = status
+
+            if status == 'AC':
+                userprof.totalScore += 100
+                question.totalSuccessfulSub += 1
+                question.totalSub += 1
+                sub.subScore = 100
+                mulque.scoreQuestion = 100
+                userprof.latestSubTime = subTime
+
+            else:
+                question.totalSub += 1
+                sub.subScore = 0
+                mulque.scoreQuestion = 0
+
+            try:
+                question.accuracy = round((question.totalSuccessfulSub * 100 / question.totalSub), 1)
+            except ZeroDivisionError:
+                question.accuracy = 0
+
+            question.save()
+            userprof.save()
+            mulque.save()
+
+            var = calculate()
+
+            dict = {
+                "testcases": testcase_values,
+                "status": sub.subStatus,
+                "error": error_text,
+                "score": sub.subStatus,
+                "time": var
+            }
+
+            return JsonResponse(dict)
 
         else:
-            question.totalSub += 1
-            sub.subScore = 0
-            mulque.scoreQuestion = 0
-
-        try:
-            question.accuracy = round((question.totalSuccessfulSub * 100 / question.totalSub), 1)
-        except ZeroDivisionError:
-            question.accuracy = 0
-
-        question.save()
-        userprof.save()
-        mulque.save()
-        dict = {
-            "testcases" : testcase_values,
-            "status" : sub.subStatus,
-            "error" : error_text,
-            "score" : sub.subStatus,
-        }
-
-        return JsonResponse(dict)
+            return render(reverse('signup'))
 
 
 class LeaderBoard(APIView):
-    def latest(self, data):
-        player = UserProfile.objects.filter(user=data['user'])
-        return player.latestSubTime
-
     def get(self, request):
-        data = list()
-        for player in UserProfile.objects.order_by("-totalScore"):
-            l = {}
-            l['user']=player.user.username
-            for i in range(1, 7):
-                que = Question.objects.get(pk=i)
-                try:
-                    ans = MultipleQues.objects.get(user=player.user, que=que)
-                    l[f'q{i}'] = ans.scoreQuestion
-                except MultipleQues.DoesNotExist:
-                    l[f'q{i}'] = 0
+        if request.user.is_authenticated:
+            data = list()
+            for player in UserProfile.objects.order_by("-totalScore", "latestSubTime"):
+                l = {}
+                l['user']=player.user.username
+                for i in range(1, 7):
+                    que = Question.objects.get(pk=i)
+                    try:
+                        ans = MultipleQues.objects.get(user=player.user, que=que)
+                        l[f'q{i}'] = ans.scoreQuestion
+                    except MultipleQues.DoesNotExist:
+                        l[f'q{i}'] = 0
 
-            l['total'] = player.totalScore
-            l['color'] = "nonTrans"
+                l['total'] = player.totalScore
+                l['color'] = "nonTrans"
 
-            var = calculate()
-            l['time'] = var
-            data.append(l)
-
-        return Response(data)
+                var = calculate()
+                l['time'] = var
+                data.append(l)
+            return Response(data)
+        else:
+            return render(reverse('signup'))
 
 
 class Submissions(APIView):
-    permission_classes = [IsAuthenticated]
-
     def get(self, request, qn):
-        que = Question.objects.get(pk=qn)
-        total_submissions = Submission.objects.filter(user=request.user)
-        usersub = []
+        if request.user.is_authenticated:
+            que = Question.objects.get(pk=qn)
+            total_submissions = Submission.objects.filter(user=request.user)
+            usersub = []
 
-        for submission in total_submissions:
-            if submission.que == que:
-                usersub.append(submission)
+            for submission in total_submissions:
+                if submission.que == que:
+                    usersub.append(submission)
 
-        var = calculate()
-        return Response({"submissions": usersub, "time": var})
+            var = calculate()
+            return Response({"submissions": usersub, "time": var})
+        else:
+            return render(reverse('signup'))
 
 
 class Questionhub(APIView):
     def get(self, request):
-        all_questions = Question.objects.all()
-        data = []
+        if request.user.is_authenticated:
+            all_questions = Question.objects.all()
+            data = []
 
-        for que in all_questions:
-            detail = {
-                "question_title": que.titleQue,
-                "accuracy": que.accuracy,
-                "submissions": que.totalSuccessfulSub
-            }
-            data.append(detail)
-        return JsonResponse(data, safe=False)
+            var = calculate()
+
+            for que in all_questions:
+                detail = {
+                    "question_title": que.titleQue,
+                    "accuracy": que.accuracy,
+                    "submissions": que.totalSuccessfulSub
+                }
+                data.append(detail)
+
+            dict = {"data": data, "time": var}
+            return JsonResponse(dict)
+        else:
+            return render(reverse('signup'))
 
 
 class Result(APIView):
-    def get(self):
-        l = []
-        d = {}
-        for i in range(1,7):
-            d["score{}".format(i)] = 0
-        userprof = UserProfile.objects.all()
-        for user in userprof:
-            if user.totalScore <=100:
-                d["score{}".format((user.totalScore-user.totalScore)+1)] = d["score{}".format((user.totalScore-user.totalScore)+1)] +1
-            elif user.totalScore <=200:
-                d["score{}".format((user.totalScore-user.totalScore)+2)] = d["score{}".format((user.totalScore-user.totalScore)+2)] +1
-            elif user.totalScore <=300:
-                d["score{}".format((user.totalScore-user.totalScore)+3)] = d["score{}".format((user.totalScore-user.totalScore)+3)] +1
-            elif user.totalScore <=400:
-                d["score{}".format((user.totalScore-user.totalScore)+4)] = d["score{}".format((user.totalScore-user.totalScore)+4)] +1
-            elif user.totalScore <=500:
-                d["score{}".format((user.totalScore-user.totalScore)+5)] = d["score{}".format((user.totalScore-user.totalScore)+5)] +1
-            elif user.totalScore <=600:
-                d["score{}".format((user.totalScore-user.totalScore)+6)] = d["score{}".format((user.totalScore-user.totalScore)+6)] +1
+    def get(self,request):
+        if request.user.is_authenticated:
+            l = []
+            d = {}
+            for i in range(1,7):
+                d["score{}".format(i)] = 0
 
-        for i in range(1,7):
-            data = {}
-            data['id'] = i
-            data['range'] = "{}-{}".format((i-1)*100, i*100)
-            data['usrs'] = d['score{}'.format(i)]
+            userprof = UserProfile.objects.all()
+            for user in userprof:
+                if user.totalScore <=100:
+                    d["score{}".format((user.totalScore-user.totalScore)+1)] = d["score{}".format((user.totalScore-user.totalScore)+1)] +1
+                elif user.totalScore <=200:
+                    d["score{}".format((user.totalScore-user.totalScore)+2)] = d["score{}".format((user.totalScore-user.totalScore)+2)] +1
+                elif user.totalScore <=300:
+                    d["score{}".format((user.totalScore-user.totalScore)+3)] = d["score{}".format((user.totalScore-user.totalScore)+3)] +1
+                elif user.totalScore <=400:
+                    d["score{}".format((user.totalScore-user.totalScore)+4)] = d["score{}".format((user.totalScore-user.totalScore)+4)] +1
+                elif user.totalScore <=500:
+                    d["score{}".format((user.totalScore-user.totalScore)+5)] = d["score{}".format((user.totalScore-user.totalScore)+5)] +1
+                elif user.totalScore <=600:
+                    d["score{}".format((user.totalScore-user.totalScore)+6)] = d["score{}".format((user.totalScore-user.totalScore)+6)] +1
+
+            for i in range(1,7):
+                data = {}
+                data['id'] = i
+                data['range'] = "{}-{}".format((i-1)*100, i*100)
+                data['usrs'] = d['score{}'.format(i)]
+
+                l.append(data)
 
             var = calculate()
-            data['time'] = var
-            l.append(data)
-
-        return JsonResponse(l, safe=False)
+            dict = {
+                "data": l,
+                "time": var
+            }
+            return JsonResponse(dict)
+        else:
+            return render(reverse('signup'))
 
 
 def change_file_content(content, code_file):
