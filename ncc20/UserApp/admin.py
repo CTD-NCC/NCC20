2d48cd6dece9b228f04ca382d1c20fa2ab9dc2d5
gauravghati <gauravghati225@gmail.com>
2020-04-28 18:53:52 +0530

    clear

--- ncc20/UserApp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Question)
-admin.site.register(Submission)
-admin.site.register(UserProfile)
-admin.site.register(MultipleQues)
-
-# Register your models here.
--- ncc20/frontend/src/reducers/url.js
+++
@@ -1,7 +0,0 @@
-const urlReducer = (state = {
-url : "127.0.0.1:8000"
-},action) => {
-    return state;
-}
-
-export default urlReducer;
